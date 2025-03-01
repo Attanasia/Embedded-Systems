b8190390243cc2cced29ccfbe04c91acb08bf5ad
Attanasia Garuso <163341589+Attanasia@users.noreply.github.com>
2025-02-28 21:35:34 -0500

    Add files via upload

---
+++ 7SegmentDisplay/SevSegmentDisplayGame.ino
@@ -0,0 +1,142 @@
+class SevenSegmentDisplay {
+private:
+    // Pin definitions for the 7-segment display
+    int pina = 3;    
+    int pinb = 4;    
+    int pinc = 5;   
+    int pind = 6;    
+    int pine = 7;    
+    int pinf = 8;    
+    int ping = 9;   
+    int pindp = 10; 
+    int startButton = 2;
+    int stopButton = 11; 
+
+    int currentValue = 0;
+    bool isRunning = false;
+    
+    // Turn all segments off
+    void clearLED() {
+        digitalWrite(pina, LOW);
+        digitalWrite(pinb, LOW);
+        digitalWrite(pinc, LOW);
+        digitalWrite(pind, LOW);
+        digitalWrite(pine, LOW);
+        digitalWrite(pinf, LOW);
+        digitalWrite(ping, LOW);
+        digitalWrite(pindp, LOW);
+    }
+
+    // Write a number (0-9) to the display
+    void write(int num) {
+        bool W, X, Y, Z;
+        bool a, b, c, d, e, f, g;
+
+        // Extract binary bits from the number
+        Z = num & 1;
+        num = num >> 1;
+        Y = num & 1;
+        num = num >> 1;
+        X = num & 1;
+        num = num >> 1;
+        W = num & 1;
+
+        // K-mapping for segment control (a-g)
+        a = W + Y + X * Z + !X * !Z;
+        b = !X + !Y * !Z + Y * Z;
+        c = X + !Y + Z;
+        d = W + !X * !Z + !X * Y + Y * !Z + X * !Y * Z;
+        e = !X * !Z + Y * !Z;
+        f = W + X * !Y + X * !Z + !Y * !Z;
+        g = W + X * !Y + !X * Y + Y * !Z;
+
+        // Set segment pins
+        digitalWrite(pina, a);
+        digitalWrite(pinb, b);
+        digitalWrite(pinc, c);
+        digitalWrite(pind, d);
+        digitalWrite(pine, e);
+        digitalWrite(pinf, f);
+        digitalWrite(ping, g);
+    }
+
+public:
+    
+    SevenSegmentDisplay() {
+        pinMode(pina, OUTPUT);
+        pinMode(pinb, OUTPUT);
+        pinMode(pinc, OUTPUT);
+        pinMode(pind, OUTPUT);
+        pinMode(pine, OUTPUT);
+        pinMode(pinf, OUTPUT);
+        pinMode(ping, OUTPUT);
+        pinMode(pindp, OUTPUT);
+
+        pinMode(startButton, INPUT_PULLUP);
+        pinMode(stopButton, INPUT_PULLUP);
+        
+        clearLED(); // Turn off all segments initially
+    }
+
+    // Function to display a number based on the rules
+    void displayNumber(int num) {
+        clearLED(); // Clear the display before updating
+
+        if (num > 19) {
+            // Turn on the DOT and turn off all other segments
+            digitalWrite(pindp, HIGH);
+        } else if (num < 0) {
+            // Turn off the display
+            clearLED();
+        } else if (num >= 0 && num <= 9) {
+            // Display the number (0-9)
+            write(num);
+        } else if (num >= 10 && num <= 19) {
+            // Display the ones place and turn on the DOT
+            write(num % 10); // Display the ones place
+            digitalWrite(pindp, HIGH); // Turn on the DOT
+        }
+    }
+
+    void startGame(){
+      if(isRunning) return;
+
+      isRunning = true;
+      long startTime = millis();
+      long duration = 10000;
+      int delayPeriod = 10;
+      int delayIncrement = 10;
+
+      while(millis() - startTime < duration){
+        currentValue = (currentValue + 1) % 10;
+        displayNumber(currentValue);
+        delay(delayPeriod);
+        delayPeriod += delayIncrement;
+      }
+      for(int i=0; i<=5;i++){
+        displayNumber(currentValue);
+        delay(500);
+        clearLED();
+        delay(500);
+      }
+      clearLED();
+      isRunning = false;
+    }
+
+    void checkButton(){
+      if(digitalRead(startButton) == LOW){
+        startGame();
+        delay(500);
+      }
+    }
+};
+
+SevenSegmentDisplay display;
+
+void setup() {
+     
+}
+
+void loop() {
+  display.checkButton();
+}
---
+++ 7SegmentDisplay/SevSegmentDisplayWithIntParameter.ino
@@ -0,0 +1,117 @@
+class SevenSegmentDisplay {
+private:
+    // Pin definitions for the 7-segment display
+    int pina = 3;    
+    int pinb = 4;    
+    int pinc = 5;   
+    int pind = 6;    
+    int pine = 7;    
+    int pinf = 8;    
+    int ping = 9;   
+    int pindp = 10;  
+
+    // Turn all segments off
+    void clearLED() {
+        digitalWrite(pina, LOW);
+        digitalWrite(pinb, LOW);
+        digitalWrite(pinc, LOW);
+        digitalWrite(pind, LOW);
+        digitalWrite(pine, LOW);
+        digitalWrite(pinf, LOW);
+        digitalWrite(ping, LOW);
+        digitalWrite(pindp, LOW);
+    }
+
+    // Write a number (0-9) to the display
+    void write(int num) {
+        bool W, X, Y, Z;
+        bool a, b, c, d, e, f, g;
+
+        // Extract binary bits from the number
+        Z = num & 1;
+        num = num >> 1;
+        Y = num & 1;
+        num = num >> 1;
+        X = num & 1;
+        num = num >> 1;
+        W = num & 1;
+
+        // K-mapping for segment control (a-g)
+        a = W + Y + X * Z + !X * !Z;
+        b = !X + !Y * !Z + Y * Z;
+        c = X + !Y + Z;
+        d = W + !X * !Z + !X * Y + Y * !Z + X * !Y * Z;
+        e = !X * !Z + Y * !Z;
+        f = W + X * !Y + X * !Z + !Y * !Z;
+        g = W + X * !Y + !X * Y + Y * !Z;
+
+        // Set segment pins
+        digitalWrite(pina, a);
+        digitalWrite(pinb, b);
+        digitalWrite(pinc, c);
+        digitalWrite(pind, d);
+        digitalWrite(pine, e);
+        digitalWrite(pinf, f);
+        digitalWrite(ping, g);
+    }
+
+public:
+    
+    SevenSegmentDisplay() {
+        pinMode(pina, OUTPUT);
+        pinMode(pinb, OUTPUT);
+        pinMode(pinc, OUTPUT);
+        pinMode(pind, OUTPUT);
+        pinMode(pine, OUTPUT);
+        pinMode(pinf, OUTPUT);
+        pinMode(ping, OUTPUT);
+        pinMode(pindp, OUTPUT);
+        clearLED(); // Turn off all segments initially
+    }
+
+    // Function to display a number based on the rules
+    void displayNumber(int num) {
+        clearLED(); // Clear the display before updating
+
+        if (num > 19) {
+            // Turn on the DOT and turn off all other segments
+            digitalWrite(pindp, HIGH);
+        } else if (num < 0) {
+            // Turn off the display
+            clearLED();
+        } else if (num >= 0 && num <= 9) {
+            // Display the number (0-9)
+            write(num);
+        } else if (num >= 10 && num <= 19) {
+            // Display the ones place and turn on the DOT
+            write(num % 10); // Display the ones place
+            digitalWrite(pindp, HIGH); // Turn on the DOT
+        }
+    }
+};
+
+// Test function to showcase all four behaviors
+void testSevenSegmentDisplay() {
+    SevenSegmentDisplay display;
+
+    // Test cases
+    display.displayNumber(25); // Case a: num > 19 (DOT ON, other segments OFF)
+    delay(2000);
+
+    display.displayNumber(-5); // Case b: num < 0 (Display OFF)
+    delay(2000);
+
+    display.displayNumber(7); // Case c: num in [0, 9] (Display 7)
+    delay(2000);
+
+    display.displayNumber(15); // Case d: num in [10, 19] (Display 5 with DOT ON)
+    delay(2000);
+}
+
+void setup() {
+    testSevenSegmentDisplay(); 
+}
+
+void loop() {
+    testSevenSegmentDisplay();
+}
